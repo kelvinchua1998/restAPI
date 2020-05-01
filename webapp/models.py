84df94582f27531467624beaaca60263ab5f47e2
kelvinchua1998 <kelvin_chua_1998@hotmail.com>
2020-05-01 17:28:43 +0800

    changed the timestamp to charfield

--- webapp/models.py
+++ webapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class speedtest_result(models.Model):
     logfile = models.FileField(default="",null=True,upload_to="file_uploads")
     token = models.CharField(max_length=30)
-    timestamp = models.DecimalField(decimal_places=5,max_digits=10)
+    timestamp = models.CharField(max_length=30)
 
     def __str__(self):              # figure out whats this do
         return self.timestamp
